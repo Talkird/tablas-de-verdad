39053ea994ec128e1e0e92803af093cfa0ca90c7
Juan Andrés Losauro <losaurojuan@Losauro--Laptop.local>
2022-04-14 11:01:26 -0300

    Added python file.

---
+++ tablas_de_verdad.py
@@ -0,0 +1,115 @@
+nLetras = 0
+
+while nLetras < 1 or nLetras > 3:
+    nLetras = int(input("Cantidad de letras: "))
+    if nLetras < 1 or nLetras > 3:
+        print("Cantidad de letras invalidas, ingrese un número entre 1 y 3")
+
+nReglones = 2 ** nLetras
+
+p = []
+q = []
+r = []
+
+#Lista P
+for i in range(int(nReglones / 2)):
+    p.append('V')
+    p.append('F')
+
+#Lista Q
+for i in range(int(nReglones / 4)):
+    q.append('V')
+    q.append('V')
+    q.append('F')
+    q.append('F')
+
+#Lista R
+for i in range(int(nReglones / 8)):
+    r.append('V')
+    r.append('V')
+    r.append('V')
+    r.append('V')
+    r.append('F')
+    r.append('F')
+    r.append('F')
+    r.append('F')
+
+def listaVertical(lista):
+    for item_lista in zip(lista):
+        print(item_lista)
+
+#Opone el valor de verdad
+def negacion(lista):
+
+    lista_resultado = []
+    for i in range(len(lista)):
+
+        if lista[i] == 'V':
+            lista_resultado.append('F')
+        else:
+            lista_resultado.append('V')
+
+    return(lista_resultado)
+
+#Verdadera en el caso V-V
+def conjuncion(lista1, lista2):
+
+    lista_resultado = []
+    for i in range(len(lista1)):    
+        if lista1[i] == 'V' and lista2[i] == 'V':
+            lista_resultado.append('V')
+        else:
+            lista_resultado.append('F')
+
+    return lista_resultado
+
+#Verdadera menos en el caso F-F
+def disyun_incluyente(lista1, lista2):
+
+    lista_resultado = []
+    for i in range(len(p)):    
+        if lista1[i] == 'F' and lista2[i] == 'F':
+            lista_resultado.append('F')
+        else:
+            lista_resultado.append('V')
+
+    return lista_resultado
+
+#Verdadera si al menos una es V
+def disyun_excluyente(lista1, lista2):
+
+    lista_resultado = []
+    for i in range(len(p)):    
+        if lista1[i] == 'V' and lista2[i] == 'F' or lista1[i] == 'F' and lista2[i] == 'V':
+            lista_resultado.append('V')
+        else:
+            lista_resultado.append('F')
+
+    return lista_resultado
+
+#Verdadera menos en el caso V-F
+def condicional(lista1, lista2):
+
+    lista_resultado = []
+    for i in range(len(p)):    
+        if lista1[i] == 'V' and lista2[i] == 'F':
+            lista_resultado.append('F')
+        else:
+            lista_resultado.append('V')
+
+    return lista_resultado
+
+#Verdadera cuando son iguales
+def bicondicional(lista1, lista2):
+
+    lista_resultado = []
+    for i in range(len(p)):    
+        if lista1[i] == 'V' and lista2[i] == 'V' or lista1[i] == 'F' and lista2[i] == 'F':
+            lista_resultado.append('V')
+        else:
+            lista_resultado.append('F')
+
+    return lista_resultado
+
+listaVertical( negacion(disyun_incluyente(p, negacion(p))) )
+
